ece1832daf7fd6802f80d1a8f6e22505db5fdb2a
leonardo <leonardolopes565@gmail.com>
2026-02-04 20:12:19 -0300

    component featuredPost + tailwind css grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,32 @@ import { SpinLoader } from "@/components/SpinLoader";
 import PostsList from "@/components/PostsList";
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
+import Link from "next/link";
+import Image from "next/image";
 
 export default async function Home() {
   const posts = await postRepository.findAll();
   return (
     <Container>
       <Header />
+
+      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
+        <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
+          <Image
+            className="group-hover:scale-105 transition"
+            src="/img/bryen_0.png"
+            width={1200}
+            height={720}
+            alt="titulo do post"
+          />
+        </Link>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis
+          voluptatibus eius suscipit reprehenderit maiores! Reprehenderit
+          aliquid error possimus dicta itaque tenetur. Quia, corrupti tempora
+          maxime odio perferendis iure ab vero.
+        </div>
+      </section>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
