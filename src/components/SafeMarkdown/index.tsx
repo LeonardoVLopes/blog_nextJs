75b8afa32780a22ea4099cae84c9312d2437f8f0
leonardo <leonardolopes565@gmail.com>
2026-02-18 17:17:49 -0300

    configuracao e conexao com Drizzle ORM + migrations para gerenciar as tabelas da base de dados

--- src/components/SafeMarkdown/index.tsx
+++ src/components/SafeMarkdown/index.tsx
@@ -8,10 +8,21 @@ type SafeMarkdownProps = {
 
 export function SafeMarkdown({ markdown }: SafeMarkdownProps) {
   return (
-    <div>
+    <div className="prose w-full max-w-none">
       <ReactMarkdown
         rehypePlugins={[rehypeSanitize]}
         remarkPlugins={[remarkGfm]}
+        components={{
+          table: ({ node, ...props }) => {
+            if (!node?.children) return "";
+
+            return (
+              <div className="overflow-x-auto">
+                <table className="w-full min-w-[600]" {...props} />
+              </div>
+            );
+          },
+        }}
       >
         {markdown}
       </ReactMarkdown>
---
+++ src/db/drizzle/schemas.ts
@@ -0,0 +1,18 @@
+import { InferInsertModel, InferSelectModel } from "drizzle-orm";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const postsTable = sqliteTable("posts", {
+  id: text("id").primaryKey(),
+  slug: text("slug").notNull().unique(),
+  title: text("title").notNull(),
+  author: text("author").notNull(),
+  excerpt: text("excerpt").notNull(),
+  content: text("content").notNull(),
+  coverImageUrl: text("cover_Image_Url").notNull(),
+  published: integer("published", { mode: "boolean" }).notNull(),
+  createdAt: text("created_At").notNull(),
+  updatedAt: text("updated_At").notNull(),
+});
+
+export type postsTableSelectMode = InferSelectModel<typeof postsTable>;
+export type postsTableInsertMode = InferInsertModel<typeof postsTable>;
