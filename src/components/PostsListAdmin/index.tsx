6afd1bd3257d9a7a3081b9d9b42daa479d413dd5
leonardo <leonardolopes565@gmail.com>
2026-03-05 17:19:14 -0300

    Dialog: adicionando as funcionalidades do componente

--- src/components/PostsListAdmin/index.tsx
+++ src/components/PostsListAdmin/index.tsx
@@ -4,6 +4,7 @@ import clsx from "clsx";
 import { Trash2Icon } from "lucide-react";
 import Link from "next/link";
 import { DeletePostButton } from "../Admin/DeletePostButton";
+import { Dialog } from "../Dialog";
 
 export default async function PostsListAdmin() {
   const posts = await findAllPostAdmin();
@@ -32,50 +33,6 @@ export default async function PostsListAdmin() {
           </div>
         );
       })}
-
-      <div
-        className={clsx(
-          "fixed z-50 inset-0 bg-black/50 backdrop-blur-xs",
-          "flex items-center justify-center",
-        )}
-      >
-        <div
-          className={clsx(
-            "bg-slate-100 p-6 rounded-lg max-w-2xl mx-6 items-center",
-            "flex flex-col gap-6",
-            'shadow-lg shadow-black/30'
-          )}
-        >
-          <h3 className="text-xl font-extrabold">Titulo do dialogo</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-            minus consectetur quos, quae quasi mollitia perferendis incidunt
-            temporibus, fugit dicta quibusdam provident. Ullam quam et in qui
-            impedit distinctio culpa.
-          </p>
-          <div className="flex items-center justify-around">
-            <button
-              className={clsx(
-                "bg-slate-300 hover:bg-slate-400 transition text-slate-950",
-                "flex items-center justify-center",
-                "py-2 px-4 rounded-lg cursor-pointer",
-              )}
-              autoFocus
-            >
-              Cancelar
-            </button>
-            <button
-              className={clsx(
-                "bg-blue-500 hover:bg-blue-600 transition text-blue-50",
-                "flex items-center justify-center",
-                "py-2 px-4 rounded-lg cursor-pointer",
-              )}
-            >
-              Ok
-            </button>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
