876a5f60034d273fec316ef769b9df9025483997
leonardo <leonardolopes565@gmail.com>
2026-03-11 20:55:23 -0300

    Sobre a criação dos componentes de Form com Tailwind CSS, clsx e JSX

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,3 +1,7 @@
+import { Button } from "@/components/Button";
+
 export default async function AdminPostNewage() {
-  return <div className="">AdminPostNewPage</div>;
+  return <div className="py-16">AdminPostNewPage
+  <Button type="submit"/>
+  </div>;
 }
