2975f2791c51c2d35de83216ad306a38f58634a1
leonardo <leonardolopes565@gmail.com>
2026-04-06 20:16:53 -0300

    Formulário de criação e edição do post (ManagePostForm)

---
+++ src/components/Admin/ManagePostForm/index.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Button } from "@/components/Button";
+import { InputCheckBox } from "@/components/InputCheckBox";
+import { InputText } from "@/components/InputText";
+
+export function ManagePostForm() {
+  return (
+    <form action="" className="mb-16">
+      <div className="flex flex-col gap-6">
+        <InputText labelText="Nome" placeholder="Digite seu nome" />
+        <InputText labelText="Sobrenome" placeholder="Digite seu sobrenome" />
+        <InputCheckBox labelText="sobrenome" />
+        <InputText
+          disabled
+          labelText="digite seu nome"
+          placeholder="Digite seu sobrenome"
+        />
+        <InputText
+          disabled
+          labelText="Sobrenome"
+          placeholder="Digite seu sobrenome"
+        />
+
+        <div className="mt-4">
+          <Button type="submit">Enviar</Button>
+        </div>
+      </div>
+    </form>
+  );
+}
