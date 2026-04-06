8708bc63759ec9db34e0c9e2f34811470f3c97a3
leonardo <leonardolopes565@gmail.com>
2026-04-06 20:57:36 -0300

    Criando o editor Markdown com @uiw/react-md-editor

--- src/components/Admin/ManagePostForm/index.tsx
+++ src/components/Admin/ManagePostForm/index.tsx
@@ -3,8 +3,12 @@
 import { Button } from "@/components/Button";
 import { InputCheckBox } from "@/components/InputCheckBox";
 import { InputText } from "@/components/InputText";
+import { MarkdownEditor } from "@/components/MarkdownEditor";
+import { useState } from "react";
 
 export function ManagePostForm() {
+  const [contentValue, setContentValue] = useState("este e um exemplo");
+
   return (
     <form action="" className="mb-16">
       <div className="flex flex-col gap-6">
@@ -16,6 +20,15 @@ export function ManagePostForm() {
           labelText="digite seu nome"
           placeholder="Digite seu sobrenome"
         />
+
+        <MarkdownEditor
+          labelText="conteudo"
+          disabled={false}
+          textAreaName="content"
+          value={contentValue}
+          setValue={setContentValue} 
+        />
+
         <InputText
           disabled
           labelText="Sobrenome"
