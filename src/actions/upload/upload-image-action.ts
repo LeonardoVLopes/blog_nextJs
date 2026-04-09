c407a159607061fbda497a0acd452b27cb1103fe
leonardo <leonardolopes565@gmail.com>
2026-04-09 18:14:58 -0300

    Validando o arquivo de imagem enviado ao servidor

--- src/actions/upload/upload-image-action.ts
+++ src/actions/upload/upload-image-action.ts
@@ -1,11 +1,34 @@
-'use server'
+"use server";
 
-import { logColor } from "@/utils/log-color"
+import { IMAGE_UPLOAD_MAX_SIZE } from "@/lib/constants";
+import { logColor } from "@/utils/log-color";
+import { error } from "console";
 
-export async function uploadImageAction() {
-    logColor('action de uploadImage')
+type uploadImageActionResult = {
+  url: string;
+  error: string;
+};
 
-    return {
-        user: 'senha do usuario',
-    }
+export async function uploadImageAction(): Promise<uploadImageActionResult> {
+  const makeResult = ({ url = "", error = "" }) => ({ url, error });
+
+  if (!(FormData instanceof FormData)) {
+    return makeResult({ error: "dados invalidos" });
+  }
+
+  const file = FormData.get("file");
+
+  if (!(file instanceof File)) {
+    return makeResult({ error: "arquivo invalido" });
+  }
+
+  if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
+    return makeResult({ error: "arquivo muito grande" });
+  }
+
+  if (file.type.startsWith("image/")) {
+    return makeResult({ error: "Image invalida" });
+  }
+
+  return makeResult({ url: "URL" });
 }
