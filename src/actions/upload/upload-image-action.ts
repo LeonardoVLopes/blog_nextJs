62c733d11db0284e209ed3952eaf222e5c374ecf
leonardo <leonardolopes565@gmail.com>
2026-04-13 19:59:40 -0300

    Salvando a imagem enviada dentro do servidor com JS + Node

--- src/actions/upload/upload-image-action.ts
+++ src/actions/upload/upload-image-action.ts
@@ -1,22 +1,24 @@
 "use server";
 
-import { IMAGE_UPLOAD_MAX_SIZE } from "@/lib/constants";
-import { logColor } from "@/utils/log-color";
-import { error } from "console";
+import {
+  IMAGE_SERVER_URL,
+  IMAGE_UPLOAD_DIRECTORY,
+  IMAGE_UPLOAD_MAX_SIZE,
+} from "@/lib/constants";
+import { mkdir, writeFile } from "fs/promises";
+import { extname, resolve } from "path";
 
 type uploadImageActionResult = {
   url: string;
   error: string;
 };
 
-export async function uploadImageAction(): Promise<uploadImageActionResult> {
+export async function uploadImageAction(
+  formData: FormData,
+): Promise<uploadImageActionResult> {
   const makeResult = ({ url = "", error = "" }) => ({ url, error });
 
-  if (!(FormData instanceof FormData)) {
-    return makeResult({ error: "dados invalidos" });
-  }
-
-  const file = FormData.get("file");
+  const file = formData.get("file");
 
   if (!(file instanceof File)) {
     return makeResult({ error: "arquivo invalido" });
@@ -26,9 +28,28 @@ export async function uploadImageAction(): Promise<uploadImageActionResult> {
     return makeResult({ error: "arquivo muito grande" });
   }
 
-  if (file.type.startsWith("image/")) {
+  if (!file.type.startsWith("image/")) {
     return makeResult({ error: "Image invalida" });
   }
+
+  const imageExtension = extname(file.name);
+  const uniqueImageName = `${Date.now()}${imageExtension}`;
+
+  const uploadFullPath = resolve(
+    process.cwd(),
+    "public",
+    IMAGE_UPLOAD_DIRECTORY,
+  );
+  await mkdir(uploadFullPath, { recursive: true });
+
+  const fileArrayBuffer = await file.arrayBuffer();
+  const buffer = Buffer.from(fileArrayBuffer);
+
+  const fileFullPath = resolve(uploadFullPath, uniqueImageName);
+
+  await writeFile(fileFullPath, buffer);
+
+  const url = `${IMAGE_SERVER_URL}/${uniqueImageName}`;
 
   return makeResult({ url: "URL" });
 }
