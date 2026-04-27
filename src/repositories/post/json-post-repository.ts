8ee468b3440b9b85c21209b5ff5fad89348fa6cb
leonardo <leonardolopes565@gmail.com>
2026-04-27 19:37:01 -0300

    Revisão, limpeza e pequenas correções no código

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -1,8 +1,9 @@
 import { postModel } from "@/models/post/post-model";
 import { PostRepository } from "./post-repository";
 import { resolve } from "path";
-import { readFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 import { SIMULATE_WAIT_IN_MS } from "@/lib/constants";
+import RootErrorPage from "@/app/error";
 
 const ROOT_DIR = process.cwd();
 const JSON_POST_FILE_PATH = resolve(
@@ -25,6 +26,11 @@ export class JsonPostRepository implements PostRepository {
     const parsedJson = JSON.parse(jsonContent);
     const { posts } = parsedJson;
     return posts;
+  }
+
+  private async writeToDisk(posts: postModel[]): Promise<void> {
+    const jsonToString = JSON.stringify({ posts }, null, 2);
+    await writeFile(JSON_POST_FILE_PATH, jsonToString, "utf-8");
   }
 
   async findAllPublic(): Promise<postModel[]> {
@@ -52,5 +58,64 @@ export class JsonPostRepository implements PostRepository {
     if (!post) throw new Error("post nao encontrado para slug");
     return post;
   }
-}
 
+  async create(post: postModel): Promise<postModel> {
+    const posts = await this.findAll();
+
+    if (!post.id || !post.slug) {
+      throw new Error("Post sem ID ou Slug");
+    }
+
+    const idOrSlugExist = posts.find(
+      (savedPost) => savedPost.id === post.id || savedPost.slug === post.slug,
+    );
+
+    if (idOrSlugExist) {
+      throw new Error("ID ou Slug devem ser Unicos");
+    }
+
+    posts.push(post);
+    await this.writeToDisk(posts);
+
+    return post;
+  }
+
+  async delete(id: string): Promise<postModel> {
+    const posts = await this.findAll();
+    const postIndex = posts.findIndex((p) => p.id === id);
+
+    if (postIndex < 0) {
+      throw new Error("Post nao existe");
+    }
+
+    const post = posts[postIndex];
+    posts.slice(postIndex, 1);
+    await this.writeToDisk(posts);
+
+    return post;
+  }
+
+  async update(
+    id: string,
+    newPostData: Omit<postModel, "id" | "slug" | "createdAt" | "updatedAt">,
+  ): Promise<postModel> {
+    const posts = await this.findAll();
+    const postIndex = posts.findIndex((p) => p.id === id);
+    const savedPost = posts[postIndex];
+
+    if (postIndex < 0) {
+      throw new Error("Post nao existe");
+    }
+
+    const newPost = {
+      ...savedPost,
+      ...newPostData,
+      updatedAt: new Date().toISOString(),
+    };
+
+    posts[postIndex] = newPost;
+    await this.writeToDisk(posts);
+
+    return newPost;
+  }
+}
