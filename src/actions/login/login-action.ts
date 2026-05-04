fa88b95b36faa2cd1ffa296c84ff37c2dcd8d94e
leonardo <leonardolopes565@gmail.com>
2026-05-04 19:37:03 -0300

    Gerando e comparando hash de senha com Bcrypt para segurança

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -11,6 +11,21 @@ type loginActionState = {
 export async function loginAction(state: loginActionState, formData: FormData) {
   await asyncDelay(5000);
 
+  if (!(formData instanceof FormData)) {
+    return {
+      username: "",
+      error: "Dados invalidos",
+    };
+  }
+
+  // dados que o usuario digitou no form
+  const username = formData.get("username")?.toString() || "";
+  const password = formData.get("password")?.toString() || "";
+
+  // aqui eu verifico se o usuario existe na base de dados
+  const isUsernameValid = username === process.env.LOGIN_USER;
+  const isPasswordValid = password ===
+
   return {
     username: "",
     error: "",
